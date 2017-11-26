dc9cb7f87b6600c468c47ee12e2acbe10fbcc371
fcatch <haopeng.uc@gmail.com>
2017-11-25 23:03:04 -0600

    init

---
+++ HB3/HB3-trigger.sh
@@ -0,0 +1,95 @@
+hadoop1_dir=hbase-1
+hadoop2_dir=hbase-2
+
+echo  
+echo "*******************************************************************"
+echo " [1]Initialization: clean if last run data exists ... "
+echo "*******************************************************************"
+echo 
+
+rm -rf hbase-1/logs/*
+rm -rf hbase-2/logs/*
+rm -rf /tmp/data-1
+rm -rf /tmp/data-2
+
+cp -r ./data/data-1 /tmp/
+cp -r ./data/data-2 /tmp/
+
+echo -e "0\n" > /tmp/HB3-crash.flag
+
+echo 
+echo "*******************************************************************"
+echo " [2]Start: run master cluster hbase-1 and slave cluster hbase-2 ... " 
+echo "*******************************************************************"
+echo 
+
+$hadoop1_dir/bin/start-hbase.sh
+
+$hadoop1_dir/bin/local-regionservers.sh start 1
+
+$hadoop2_dir/bin/start-hbase.sh
+
+$hadoop2_dir/bin/local-regionservers.sh start 2
+
+echo -n "Starting slave cluster: hbase-2"
+
+  while [ true ]
+   do
+     sleep 1; 
+     startComplete=`grep "org.apache.hadoop.hbase.master.HMaster: Master has completed initialization" $hadoop2_dir/logs/hbase-fcatch-master-ubuntu.log`
+     if [ "$startComplete" != "" ];then
+       echo -e "\nCheck: Slave cluster hbase-2 has completed initialization."
+       break
+     else
+       echo -n "."
+     fi
+   done
+
+   
+echo -n "Starting master cluster: hbase-1"
+  
+  while [ true ]
+   do
+     sleep 1; 
+     startComplete=`grep "org.apache.hadoop.hbase.master.HMaster: Master has completed initialization" $hadoop1_dir/logs/hbase-fcatch-master-ubuntu.log`
+     if [ "$startComplete" != "" ];then
+       echo "Check: Master cluster hbase-1 has completed initialization."
+       break
+     else
+       echo -n "."
+     fi
+      rsCrashed=`grep "Crash Here After Deleting" $hadoop1_dir/logs/hbase-fcatch-1-regionserver-ubuntu.log`
+      if [ "$rsCrashed" != "" ];then
+         echo -e "\n"$rsCrashed
+         echo "Master cluster hbase-1: RegionServer 1 has crashed, restart and recover it"
+         $hadoop1_dir/bin/local-regionservers.sh start 1
+         break
+       fi
+   done
+
+
+echo 
+echo "*******************************************************************"
+echo " [3]Data Status: show data in master and slave clusters ..."
+echo "*******************************************************************"
+echo 
+
+./data/showData.sh
+
+echo
+echo "It can be found that the data of table hb5 in master cluster fails to migrate to slave cluster due to crash"
+echo 
+echo "*******************************************************************"
+echo " [4]Stop: shutdown master cluster to imitate cluster outage and then show data again..."
+echo "*******************************************************************"
+echo 
+
+echo "Shutdown master cluster"
+
+$hadoop1_dir/bin/stop-hbase.sh
+
+./data/showData.sh
+
+echo
+echo "It can be found that the data of table hb5 is lost in both master and slave clusters"
+echo
---
+++ MR5/MR5-trigger.sh
@@ -0,0 +1,33 @@
+prefix=/home/fcatch/MR5
+
+mkdir -p $prefix/temp/dfs/data
+mkdir -p $prefix/temp/dfs/name
+
+config="--config $prefix/hadoop-config"
+hadoop=$prefix/hadoop-2.1.0-beta-src/hadoop-dist/target/hadoop-2.1.0-beta/
+
+$hadoop/bin/hdfs $config namenode -format 2>/dev/null
+$hadoop/sbin/start-dfs.sh $config 2>/dev/null
+
+$hadoop/bin/hdfs $config dfs -mkdir /input 2>/dev/null
+$hadoop/bin/hdfs $config dfs -put $hadoop/etc/hadoop/httpfs-site.xml /input 2>/dev/null
+$hadoop/bin/hdfs $config dfs -put $hadoop/etc/hadoop/yarn.xml /input 2>/dev/null
+$hadoop/bin/hdfs $config dfs -put $hadoop/etc/hadoop/core-site.xml /input 2>/dev/null
+
+$hadoop/sbin/start-yarn.sh $config 2>/dev/null
+
+if [ ! -f "/tmp/MR5-crash.flag" ]; then
+  touch /tmp/MR5-crash.flag
+fi
+
+echo "Running wordcount job"
+$hadoop/bin/hadoop $config jar $hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.1.0-beta.jar wordcount /input /output 2> /dev/null
+
+echo "Job status:"
+sleep 1
+$hadoop/bin/hadoop $config job -list all 2>/dev/null| grep -iE "^\s+job" | cut -d$'\t' -f1,2,3,4
+
+$hadoop/sbin/stop-yarn.sh $config 2>/dev/null 1>&2
+$hadoop/sbin/stop-dfs.sh $config 2>/dev/null 1>&2
+
+rm -rf $prefix/temp
